b4494a913f7bafe6f6f0ca78f2b0883826c403bd
Mike Kravtsov <mike4040@me.com>
2020-06-05 15:34:18 -0700

    connected to db

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+const query = 'SELECT table_schema,table_name FROM information_schema.tables;';
+
+client.query(
+  query,
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
