97d8aaae9e473de203b93504d4db14bd5e871bc9
Mike Kravtsov <mike4040@me.com>
2020-06-07 13:52:04 -0700

    typos

--- db/addUserToGroup.js
+++ db/addUserToGroup.js
@@ -1,12 +1,12 @@
 const db = require('./index');
 
-const addUserToGroup = async (user_id, group_id) => {
+const addUserToGroup = async (userId, groupId) => {
   try {
     const query = `INSERT INTO public.user_in_group
     (user_id, group_id)
     VALUES ( $1, $2)
     RETURNING id;`;
-    const values = [user_id, group_id];
+    const values = [userId, groupId];
     const { rows } = await db.query(query, values);
     return { recordId: rows[0].id };
   } catch (err) {
--- db/createAccount.js
+++ db/createAccount.js
@@ -2,14 +2,14 @@ const db = require('./index');
 
 const createAccount = async (email, name, hash) => {
   try {
-     const query = `INSERT INTO public.user (name, email, password)
+    const query = `INSERT INTO public.user (name, email, password)
          VALUES ( $1, $2, $3)
          RETURNING id;`;
-     const values = [name, email, hash];
+    const values = [name, email, hash];
     const { rows } = await db.query(query, values);
     return { id: rows[0].id };
   } catch (err) {
-    console.log('Add User', err);
+    console.log('createAccount', err);
     return { err: err.detail || err.code };
   }
 };
