f401b8980409492ddff02168cf19f7f08b93ae41
Mike Kravtsov <mike4040@me.com>
2020-06-08 08:20:55 -0700

    signin refactored

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const db = require('../db');
-const updateUser = require('../db/updateUser');
 const createAccount = require('../db/createAccount');
+const getUserByEmail = require('../db/getUserByMail');
+const updateUser = require('../db/updateUser');
 const { checkPassword, issueToken, hashPassword } = require('../utils/auth');
 const { messages } = require('../configrc');
 
@@ -8,31 +9,20 @@ module.exports = {
   signin: async ({ body }, res) => {
     /** extra layer, body is validated already */
     if (!body.password) return res.json({ msg: messages.passRequred });
-    const query = `SELECT id, name, email, password
-                   FROM public.user
-                   WHERE email = $1`;
-    const values = [body.email];
     try {
-      const result = await db.query(query, values);
-      const dbUser = result.rows[0];
-      if (dbUser && dbUser.password) {
-        if (checkPassword(body.password, dbUser.password))
-          return res.json({
-            msg: messages.loggedIn,
-            token: issueToken(dbUser),
-          });
-        else
-          return res.json({
-            msg: messages.wrongPass,
-          });
-      } else {
-        return res.json({
-          msg: messages.noAccount,
-        });
-      }
+      const { user: dbUser, err } = await getUserByEmail(body.email);
+      if (err) return res.json({ msg: messages.dbError, err });
+      if (!(dbUser && dbUser.password))
+        return res.json({ msg: messages.noAccount });
+      if (!checkPassword(body.password, dbUser.password))
+        return res.json({ msg: messages.wrongPass });
+      return res.json({
+        msg: messages.loggedIn,
+        token: issueToken(dbUser),
+      });
     } catch (err) {
-      console.log(err.detail);
-      return res.status(500).send(err.detail);
+      console.log(err);
+      return res.status(500).send(err);
     }
   },
   signup: async ({ body }, res) => {
@@ -45,6 +35,7 @@ module.exports = {
     try {
       const result = await db.query(query, values);
       const dbUser = result.rows[0];
+      
       if (dbUser) {
         /** @todo add checking User Name against db */
         if (dbUser.password)
