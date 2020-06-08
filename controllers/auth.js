2d41d12269e7aa1cb4fce136b90561adde8b08c2
Mike Kravtsov <mike4040@me.com>
2020-06-08 08:43:18 -0700

    signup refactored

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,3 @@
-const db = require('../db');
 const createAccount = require('../db/createAccount');
 const getUserByEmail = require('../db/getUserByMail');
 const updateUser = require('../db/updateUser');
@@ -12,10 +11,8 @@ module.exports = {
     try {
       const { user: dbUser, err } = await getUserByEmail(body.email);
       if (err) return res.json({ msg: messages.dbError, err });
-      if (!(dbUser && dbUser.password))
-        return res.json({ msg: messages.noAccount });
-      if (!checkPassword(body.password, dbUser.password))
-        return res.json({ msg: messages.wrongPass });
+      if (!(dbUser && dbUser.password)) return res.json({ msg: messages.noAccount });
+      if (!checkPassword(body.password, dbUser.password)) return res.json({ msg: messages.wrongPass });
       return res.json({
         msg: messages.loggedIn,
         token: issueToken(dbUser),
@@ -28,47 +25,44 @@ module.exports = {
   signup: async ({ body }, res) => {
     /** extra layer, body is validated already */
     if (!body.password) return res.json({ msg: messages.passRequred });
-    const query = `SELECT id, name, email, password
-                   FROM public.user
-                   WHERE email = $1`;
-    const values = [body.email];
     try {
-      const result = await db.query(query, values);
-      const dbUser = result.rows[0];
-      
+      const { user: dbUser, err: err1 } = await getUserByEmail(body.email);
+      if (err1) return res.json({ msg: messages.dbError, err: err1 });
+
       if (dbUser) {
         /** @todo add checking User Name against db */
         if (dbUser.password)
-          if (checkPassword(body.password, dbUser.password))
-            return res.json({
-              msg: messages.accExistsLoggedIn,
-              token: issueToken(dbUser),
-            });
-          else
-            return res.json({
-              msg: messages.accExistsWrongPass,
-            });
-        else {
-          const { id, err } = await updateUser(hashPassword(body.password), body.name, dbUser.id);
-          if (err) return res.json({ msg: messages.dbError, err });
-          return res.json({
-            msg: messages.accCreated,
-            token: issueToken({ ...body, id }),
-          });
-        }
+          return res.json(
+            checkPassword(body.password, dbUser.password)
+              ? {
+                  msg: messages.accExistsLoggedIn,
+                  token: issueToken(dbUser),
+                }
+              : {
+                  msg: messages.accExistsWrongPass,
+                }
+          );
+
+        const { id, err2 } = await updateUser(hashPassword(body.password), body.name, dbUser.id);
+        if (err2) return res.json({ msg: messages.dbError, err: err2 });
+
+        return res.json({
+          msg: messages.accCreated,
+          token: issueToken({ ...body, id }),
+        });
       }
 
       /** no user in db, creating an account */
-
       const { id, err } = await createAccount(body.email, body.name, hashPassword(body.password));
       if (err) return res.json({ msg: messages.dbError, err });
+
       return res.json({
         msg: messages.accCreated,
         token: issueToken({ ...body, id }),
       });
     } catch (err) {
-      console.log(err.detail);
-      return res.status(500).send(err.detail);
+      console.log(err);
+      return res.status(500).send(err);
     }
   },
 };
